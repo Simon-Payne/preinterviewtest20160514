b6ded1df9414d626ac0702491ae9076dab9fc134
Simon Payne <simon.payne@momogroup.co>
2016-05-14 12:24:00 +0100

    Step 2: Simple offers (initial commit without refactoring)

--- src/main/scala/com/simonpayne/hmrctest/checkout.scala
+++ src/main/scala/com/simonpayne/hmrctest/checkout.scala
@@ -2,8 +2,42 @@ package com.simonpayne.hmrctest
 
 case class GroceryItem(name: String, price: BigDecimal)
 
-class CheckoutSystem {
+case class Promotion(item: GroceryItem, isBogof: Boolean = false, isXforY: Boolean = false, x: Option[Int] = None, y: Option[Int] = None)
 
-  def addUpScannedItems(goods: List[GroceryItem]): BigDecimal = goods.map { _.price }.sum    
-  
+class CheckoutSystem(val promos: List[Promotion]) {
+
+  def addUpScannedItems(goods: List[GroceryItem]): BigDecimal = {
+
+    if (promos.isEmpty) goods.map { _.price }.sum
+    else {
+
+      if (goods.isEmpty) BigDecimal(0)
+      else {
+        val split = goods.partition { g => promos.map { _.item }.contains(g) }
+        val nonPromoTotal = split._2.map { _.price }.sum
+        val onPromotion = split._1
+
+        val furtherSplit = onPromotion.partition { g => promos.filter(_.isBogof).map { _.item }.contains(g) }
+        val onBogof = furtherSplit._1
+        val onXforY = furtherSplit._2 // let's assume for now it's 3-for-2
+
+        // bogofs        
+        val countB = onBogof.size
+        val pairs = (countB / 2).toInt
+        val remainder = (countB - pairs)
+        val price = onBogof.take(1).head.price
+        val totalBogof = price + (price * pairs) 
+
+        // 3for2                
+        val xfory = onXforY.map { _.price }.sum
+        val xForYTotal = (2 * xfory) / 3
+
+        totalBogof + nonPromoTotal + xForYTotal
+
+      }
+
+    }
+
+  }
+
 }
--- src/test/scala/com/simonpayne/hmrctest/checkoutTests.scala
+++ src/test/scala/com/simonpayne/hmrctest/checkoutTests.scala
@@ -7,27 +7,55 @@ class CheckoutSystemTests extends FlatSpec with Matchers {
   val apple = GroceryItem("Apple", BigDecimal(0.6))
   val orange = GroceryItem("Orange", BigDecimal(0.25))   
   
-  val system = new CheckoutSystem()
+  val systemNoPromos = new CheckoutSystem(Nil)
 
+  // v1 regression tests
+  
   "A checkout system" should "add up the items in a basket and output the correct total" in {    
     val basket = List(apple, apple, orange, apple) 
-    system.addUpScannedItems(basket) should be (BigDecimal(2.05))    
+    systemNoPromos.addUpScannedItems(basket) should be (BigDecimal(2.05))    
   }
   
   "A checkout system" should "handle an empty list" in {    
     val basket = List.empty 
-    system.addUpScannedItems(basket) should be (BigDecimal(0))    
+    systemNoPromos.addUpScannedItems(basket) should be (BigDecimal(0))    
   }
   
   "A checkout system" should "handle a list of incorrectly configured items (negative prices)" in {    
     val basket = List(GroceryItem("AppleWrongPrice", BigDecimal(-1)))
     try {
-      system.addUpScannedItems(basket)
+      systemNoPromos.addUpScannedItems(basket)
       fail()
     } catch {
       case e: Exception => // this is okay
     } 
   }
   
+  // v2 tests
+  
+  val bogofPromo = Promotion(apple, true)
+  val xforYPromo = Promotion(orange, false, true, Some(3), Some(2))   
+  val systemWithBogof = new CheckoutSystem(List(bogofPromo))
+  val systemWithBogofAndXforY = new CheckoutSystem(List(bogofPromo, xforYPromo))
+  
+  "A checkout system with a bogof promotion on apples" should "handle an empty list" in {    
+    val basket = List.empty 
+    systemWithBogof.addUpScannedItems(basket) should be (BigDecimal(0))    
+  }
+  
+  "A checkout system with a bogof promotion on apples" should "add up the items correctly including the bogofs and output correct total" in {    
+    val basket = List(apple, apple, orange, apple)
+    systemWithBogof.addUpScannedItems(basket) should be (BigDecimal(1.45))    
+  }
+  
+  "A checkout system with a bogof promotion on apples" should "add up the items correctly including the bogofs and output correct total - data for 3for2 test included" in {    
+    val basket = List(apple, apple, orange, apple, orange, orange)
+    systemWithBogof.addUpScannedItems(basket) should be (BigDecimal(1.95))    
+  }   
+  
+  "A checkout system with a bogof promotion on apples & 3 for 2 on oranges" should "add up the items correctly including the bogofs and output correct total - data for 3for2 test included" in {    
+    val basket = List(apple, apple, orange, apple, orange, orange)
+    systemWithBogofAndXforY.addUpScannedItems(basket) should be (BigDecimal(1.7))    
+  }
   
 }
